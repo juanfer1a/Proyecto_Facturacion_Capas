7f0ede05e8fc4be8ab19af4dae4dac7ba1c83041
Juan Quiceno <39773492+juanfer1a@users.noreply.github.com>
2022-04-07 22:48:05 -0500

    Mitad frmFactura crear Eliminar Factura

---
+++ Capa_LogicaDeNegocios/ClsFactura.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Capa_AccesoDatos;
+
+namespace Capa_LogicaDeNegocios
+{
+    class ClsFactura
+    {
+        public int C_IdFactura { get; set; }
+        public int C_DtmFecha { get; set; }
+        public int C_IdCliente { get; set; }
+        public int C_IdEmpleado { get; set; }
+        public int C_NumDescuento { get; set; }
+        public int C_NumImpuesto { get; set; }
+        public int C_NumValorTotal { get; set; }
+        public int C_IdEstado { get; set; }
+        public int C_DtmFechaModifica { get; set; }
+        public int C_StrUsuarioModifica { get; set; }
+
+        Cls_Acceso_Datos acceso = new Cls_Acceso_Datos();
+
+        public DataTable Consulta_Producto()
+        {
+            string Sentencia;
+            try
+            {
+                Sentencia = "SELECT * FROM TBLFACTURA";
+                DataTable dt = new DataTable();
+                dt = acceso.EjecutarConsulta(Sentencia);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataTable Consulta_Producto(string IdProducto)
+        {
+            string Sentencia;
+            try
+            {
+                Sentencia = $"SELECT * FROM TBLFACTURA WHERE IdProducto = '{IdProducto}'";
+                DataTable dt = new DataTable();
+                dt = acceso.EjecutarConsulta(Sentencia);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataTable Filtrar_Producto(string filtro)
+        {
+            string Sentencia;
+            try
+            {
+                Sentencia = $"SELECT * FROM TBLFACTURA WHERE strNombre LIKE '%{filtro}%'";
+                DataTable dt = new DataTable();
+                dt = acceso.EjecutarConsulta(Sentencia);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataTable Consulta_Categoria()
+        {
+            string Sentencia;
+            try
+            {
+                Sentencia = "SELECT * FROM TBLFACTURA";
+                DataTable dt = new DataTable();
+                dt = acceso.EjecutarConsulta(Sentencia);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public string Eliminar_Factura()
+        {
+            string mensaje = string.Empty;
+            try
+            {
+                //estructura tipo lista para los parametros que necesitan el procedimiento almacenado 
+                List<Cls_parametros> lst = new List<Cls_parametros>();
+
+                //
+                lst.Add(new Cls_parametros("@IdFactura", C_IdFactura));
+
+
+                mensaje = acceso.EjecutarProcedimiento("Eliminar_Producto", lst);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                mensaje = "FALLO ELIMINACION DE LA FACTURA" + ex.Message;
+            }
+
+
+            return mensaje;
+        }
+
+        public string Actulizar_Factura()
+        {
+            string mensaje = string.Empty;
+
+            try
+            {
+                List<Cls_parametros> lst = new List<Cls_parametros>();
+                lst.Add(new Cls_parametros("@IdFactura", C_IdFactura));
+                lst.Add(new Cls_parametros("@DtmFecha", C_DtmFecha));
+                lst.Add(new Cls_parametros("@IdCliente ", C_IdCliente));
+                lst.Add(new Cls_parametros("@dEmpleado ", C_IdEmpleado));
+                lst.Add(new Cls_parametros("@NumDescuento", C_NumDescuento));
+                lst.Add(new Cls_parametros("@NumImpuesto", C_NumImpuesto));
+                lst.Add(new Cls_parametros("@NumValorTotal", C_NumValorTotal));
+                lst.Add(new Cls_parametros("@IdEstado", C_IdEstado));
+                lst.Add(new Cls_parametros("@DtmFechaModifica", C_DtmFechaModifica));
+                lst.Add(new Cls_parametros("@StrUsuarioModifica", C_StrUsuarioModifica));
+
+                mensaje = acceso.EjecutarProcedimiento("actualizar_Factura", lst);
+
+            }
+            catch (Exception ex)
+            {
+
+                mensaje = "FALLO ACTULIZACION DE LA FACTURA" + ex.Message;
+            }
+
+
+            return mensaje;
+        }
+    }
+}
---
+++ Capa_LogicaDeNegocios/Cls_Productos.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Capa_AccesoDatos;
+
+
+namespace Capa_LogicaDeNegocios
+{
+    class Cls_Productos
+    {
+        public int C_IdProducto { get; set; }
+        public int C_StrNombre { get; set; }
+        public int C_StrCodigo { get; set; }
+        public int C_NumPrecioCompra { get; set; }
+        public int C_NumPrecioVenta { get; set; }
+        public int C_IdCategoria { get; set; }
+        public int C_StrDetalle { get; set; }
+        public int C_strFoto { get; set; }
+        public int C_NumStock { get; set; }
+        public int C_DtmFechaModifica { get; set; }
+        public int C_StrUsuarioModifica { get; set; }
+
+        Cls_Acceso_Datos acceso = new Cls_Acceso_Datos();
+
+        public DataTable Consulta_Producto()
+        {
+            string Sentencia;
+            try
+            {
+                Sentencia = "SELECT * FROM TBLPRODUCTO";
+                DataTable dt = new DataTable();
+                dt = acceso.EjecutarConsulta(Sentencia);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataTable Consulta_Producto(string IdProducto)
+        {
+            string Sentencia;
+            try
+            {
+                Sentencia = $"SELECT * FROM TBLPRODUCTO WHERE IdProducto = '{IdProducto}'";
+                DataTable dt = new DataTable();
+                dt = acceso.EjecutarConsulta(Sentencia);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataTable Filtrar_Producto(string filtro)
+        {
+            string Sentencia;
+            try
+            {
+                Sentencia = $"SELECT * FROM TBLPRODUCTO WHERE StrNombre LIKE '%{filtro}%'";
+                DataTable dt = new DataTable();
+                dt = acceso.EjecutarConsulta(Sentencia);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public DataTable Consulta_Categoria()
+        {
+            string Sentencia;
+            try
+            {
+                Sentencia = "SELECT * FROM TBLCATEGORIA_PROD";
+                DataTable dt = new DataTable();
+                dt = acceso.EjecutarConsulta(Sentencia);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public string Eliminar_Producto()
+        {
+            string mensaje = string.Empty;
+            try
+            {
+                //estructura tipo lista para los parametros que necesitan el procedimiento almacenado 
+                List<Cls_parametros> lst = new List<Cls_parametros>();
+
+                //
+                lst.Add(new Cls_parametros("@IdProducto", C_IdProducto));
+
+
+                mensaje = acceso.EjecutarProcedimiento("Eliminar_Producto", lst);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                mensaje = "FALLO ELIMINACION PRODUCTO" + ex.Message;
+            }
+
+
+            return mensaje;
+        }
+
+        public string Actulizar_Producto()
+        {
+            string mensaje = string.Empty;
+
+            try
+            {
+                List<Cls_parametros> lst = new List<Cls_parametros>();
+
+                lst.Add(new Cls_parametros("@IdProducto ", C_IdProducto));
+                lst.Add(new Cls_parametros("@StrNombre", C_StrNombre));
+                lst.Add(new Cls_parametros("@StrCodigo", C_StrCodigo));
+                lst.Add(new Cls_parametros("@NumPrecioCompra", C_NumPrecioCompra));
+                lst.Add(new Cls_parametros("@NumPrecioVenta", C_NumPrecioVenta));
+                lst.Add(new Cls_parametros("@IdCategoria", C_IdCategoria));
+                lst.Add(new Cls_parametros("@StrDetalle", C_StrDetalle));
+                lst.Add(new Cls_parametros("@strFoto", C_strFoto));
+                lst.Add(new Cls_parametros("@NumStock", C_NumStock));
+                lst.Add(new Cls_parametros("@DtmFechaModifica", C_DtmFechaModifica));
+                lst.Add(new Cls_parametros("@StrUsuarioModifica", C_StrUsuarioModifica));
+
+                mensaje = acceso.EjecutarProcedimiento("actualizar_Producto", lst);
+
+            }
+            catch (Exception ex)
+            {
+
+                mensaje = "FALLO ACTULIZACION DEL PRODUCTO" + ex.Message;
+            }
+
+
+            return mensaje;
+        }
+
+
+    }
+}
